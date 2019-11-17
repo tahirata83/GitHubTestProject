674dd7460b10f3ee71af4a57ee2181e419690a32
tahirata <tahiratayev@gmail.com>
2019-11-17 22:01:29 +0300

    brunch 1

--- src/day33/TestingSettings.java
+++ src/day33/TestingSettings.java
@@ -5,7 +5,7 @@ public class TestingSettings {
     public static void main(String[] args){
 
         System.out.println("cool");
-        System.out.println("Tahir");
+        System.out.println("Tahi");
     }
 }
 
