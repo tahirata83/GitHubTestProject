a6d826a692effcdebfd3be6f94d2de22af32de6b
tahirata <tahiratayev@gmail.com>
2019-11-17 22:12:38 +0300

    brunch 12r

--- src/day33/Main.java
+++ src/day33/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("ata");
-        System.out.println("Tata");
+        System.out.println("Tat");
     }
 }
 
